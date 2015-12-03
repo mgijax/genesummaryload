fb97c9af4159989809cac777352a384f5deccbcc
MGI Admin <mgiadmin@jax.org>
2015-12-03 11:36:23 +0000

    genesummaryload-6-0-3-1

---
+++ bin/createInputFile.py
@@ -0,0 +1,155 @@
+#!/usr/local/bin/python
+
+##########################################################################
+#
+# Purpose:
+#       From mygene input file create assocload input file
+#
+# Usage: createInputFile.py
+# Env Vars:
+#	 1. 
+#
+# Inputs:
+#	1. mygene.xml
+#	2. Configuration (see genesummaryload.config, assocload.config)
+#
+# Outputs:
+#	 1. tab delimited file in assocload format:
+#	    line 1 header: "MGI" "MyGene"
+# 	    1. MGI ID
+#           2. MyGene gene name
+#	 2. log file
+# 
+# Exit Codes:
+#
+#      0:  Successful completion
+#      1:  An exception occurred
+#
+#  Assumes:  Nothing
+#
+#  Notes:  None
+#
+###########################################################################
+
+import sys
+import os
+import mgi_utils
+import string
+import db
+
+print '%s' % mgi_utils.date()
+
+# MyGene logical DB Name
+LDBName = os.environ['ASSOC_EXTERNAL_LDB']
+
+# paths to input and two output files
+inFilePath = os.environ['INPUT_FILE_DEFAULT']
+assocFilePath= os.environ['INPUT_FILE']
+
+# curation log
+fpCur = open (os.environ['LOG_CUR'], 'a')
+
+# egIds mapped to organism of associated markers in MGI
+#{egId:organism, ...}
+egOrgDict = {}
+
+# curator log lists; these records will be reported and skipped
+# The MyGene egId is not associated with a marker in MGI
+egIdNotInMgiList = []
+
+# The MyGene egId is associated with a non-human marker in MGI
+egIdIsNotHumanList = []
+
+# The MyGene url stub is > 30 characters
+urlStubGt30CharList = []
+
+# The MyGene url stub contains commas (',')
+urlStubContainsCommaList = []
+
+# constants
+TAB= '\t'
+CRT = '\n'
+SPACE = ' '
+
+#
+# Initialize
+#
+
+jsonDict = eval(open(inFilePath).read())
+jsonList = jsonDict['hits']
+fpAssocFile = open(assocFilePath, 'w')
+
+results = db.sql('''select a.accid, o.commonName
+	from ACC_Accession a, MRK_Marker m, MGI_Organism o
+	where a._LogicalDB_key = 55
+	and a._MGIType_key = 2
+	and a._Object_key = m._Marker_key
+	and m._Organism_key = o._Organism_key''', 'auto')
+
+for r in results:
+    egOrgDict[r['accid']] = r['commonName']
+
+#
+# Process
+#
+
+# write out assocload header
+fpAssocFile.write('%s%s%s%s' % ('Entrez Gene', TAB, LDBName, CRT))
+
+for dict in jsonList:
+    egId = string.strip(str(dict['entrezgene']))
+    url_stub = string.strip(dict['wikipedia']['url_stub'])
+    if not egOrgDict.has_key(egId):
+	egIdNotInMgiList.append('%s %s' % (egId, url_stub))
+    elif egOrgDict[egId] != 'human':
+	egIdIsNotHumanList.append('%s %s %s' % (egId, url_stub, egOrgDict[egId]))
+    elif len(url_stub) > 30:
+	urlStubGt30CharList.append('%s %s' % (egId, url_stub))
+    elif ',' in url_stub:
+	urlStubContainsCommaList.append('%s %s ' % (egId, url_stub))
+    else:
+	fpAssocFile.write('%s%s%s%s' % (egId, TAB, url_stub, CRT))
+    
+#
+# Post Process
+#
+if len(egIdNotInMgiList):
+    fpCur.write('%sEntrezGene IDs not in MGI%s' % (CRT, CRT))
+    fpCur.write('EgId%sUrlStub%s' % (TAB, CRT))
+    fpCur.write('------------------------------------------------------------%s' % CRT)
+    fpCur.write(string.join(egIdNotInMgiList, CRT))
+    fpCur.write(CRT)
+    fpCur.write('Total: %s' % len(egIdNotInMgiList))
+    fpCur.write(CRT)
+
+if len(egIdIsNotHumanList):
+    fpCur.write('%sEntrezGene IDs associated with non-human gene in MGI%s' % (CRT, CRT))
+    fpCur.write('EgId%sUrlStub%s' % (TAB, CRT))
+    fpCur.write('------------------------------------------------------------%s' % CRT)
+    fpCur.write(string.join(egIdIsNotHumanList, CRT))
+    fpCur.write(CRT)
+    fpCur.write('Total: %s' % len(egIdIsNotHumanList))
+    fpCur.write(CRT)
+
+if len(urlStubGt30CharList):
+    fpCur.write('%sUrl Stub > 30 characters%s' % (CRT, CRT))
+    fpCur.write('EgId%sUrlStub%s' % (TAB, CRT))
+    fpCur.write('------------------------------------------------------------%s' % CRT)
+    fpCur.write(string.join(urlStubGt30CharList, CRT))
+    fpCur.write(CRT)
+    fpCur.write('Total: %s' % len(urlStubGt30CharList))
+    fpCur.write(CRT)
+
+if len(urlStubContainsCommaList):
+    fpCur.write('%sUrl Stub contains a comma%s' % (CRT, CRT))
+    fpCur.write('EgId%sUrlStub%s' % (TAB, CRT))
+    fpCur.write('------------------------------------------------------------%s' % CRT)
+    fpCur.write(string.join(urlStubContainsCommaList, CRT))
+    fpCur.write(CRT)
+    fpCur.write('Total: %s' % len(urlStubContainsCommaList))
+    fpCur.write(CRT)
+
+fpCur.close()
+fpAssocFile.close()
+
+print '%s' % mgi_utils.date()
---
+++ bin/genesummaryload.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+#
+#  genesummaryload.sh
+###########################################################################
+#
+#  Purpose:
+# 	This script parses input file, creates assocload
+#       input file and invokes assocload
+#
+  Usage=genesummaryload.sh
+#
+#  Env Vars:
+#
+#      See the configuration file
+#
+#  Inputs:
+#
+#      - Common configuration file -
+#               /usr/local/mgi/live/mgiconfig/master.config.sh
+#      - Load configuration file - genesummaryload.config
+#      - input file - see python script header
+#
+#
+#  Outputs:
+#
+#      - An archive file
+#      - Log files defined by the environment variables ${LOG_PROC},
+#        ${LOG_DIAG}, ${LOG_CUR} and ${LOG_VAL}
+#      - Input files for assocload
+#      - see assocload outputs
+#      - Records written to the database tables
+#      - Exceptions written to standard error
+#      - Configuration and initialization errors are written to a log file
+#        for the shell script
+#
+#  Exit Codes:
+#
+#      0:  Successful completion
+#      1:  Fatal error occurred
+#      2:  Non-fatal error occurred
+#
+#  Assumes:  Nothing
+#
+# History:
+#
+# sc	11/30/2015 - created
+#
+
+cd `dirname $0`
+LOG=`pwd`/genesummaryload.log
+rm -rf ${LOG}
+
+CONFIG_LOAD=../genesummaryload.config
+
+#
+# verify & source the configuration file
+#
+
+if [ ! -r ${CONFIG_LOAD} ]
+then
+    echo "Cannot read configuration file: ${CONFIG_LOAD}"
+    exit 1
+fi
+
+. ${CONFIG_LOAD}
+
+#
+#  Source the DLA library functions.
+#
+
+if [ "${DLAJOBSTREAMFUNC}" != "" ]
+then
+    if [ -r ${DLAJOBSTREAMFUNC} ]
+    then
+        . ${DLAJOBSTREAMFUNC}
+    else
+        echo "Cannot source DLA functions script: ${DLAJOBSTREAMFUNC}" | tee -a ${LOG}
+        exit 1
+    fi
+else
+    echo "Environment variable DLAJOBSTREAMFUNC has not been defined." | tee -a ${LOG}
+    exit 1
+fi
+
+#####################################
+#
+# Main
+#
+#####################################
+# remove logs (if not assocload logs will be appended)
+cleanDir ${LOGDIR}
+
+#
+# createArchive including OUTPUTDIR, startLog, getConfigEnv
+# sets "JOBKEY"
+
+preload ${OUTPUTDIR}
+
+# remove files from output directory
+cleanDir ${OUTPUTDIR}
+
+#
+# Create assocload input file
+#
+echo "Creating Gene Summary association load input file" >> ${LOG_DIAG}
+${GENESUMMARYLOAD}/bin/createInputFile.py >> ${LOG_DIAG}
+STAT=$?
+checkStatus ${STAT} "createInputFile.py"
+
+#
+# run association load
+#
+
+# set to full path for assocload
+CONFIG_LOAD=${GENESUMMARYLOAD}/assocload.config
+
+echo "Running Gene Summary association load" >> ${LOG_DIAG}
+echo "${ASSOCLOADER_SH} ${CONFIG_LOAD} ${JOBKEY}"
+${ASSOCLOADER_SH} ${CONFIG_LOAD} ${JOBKEY}
+STAT=$?
+checkStatus ${STAT} "${ASSOCLOADER_SH} ${CONFIG_LOAD}"
+
+#
+# run postload cleanup and email logs
+#
+shutDown
